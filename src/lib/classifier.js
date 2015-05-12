72d74c1a96af216c6dcd753230894576a7206bf1
Ken Grønnbeck <kengroenn@gmail.com>
2015-05-12 20:47:47 +0200

    With plural days and weeks

--- src/lib/classifier.js
+++ src/lib/classifier.js
@@ -3,8 +3,8 @@ var Day = require('./day');
 
 class Classifier {
   classify (email) {
-    const week = email.match(/([0-9]+)week/);
-    const day = email.match(/([0-9]+)day/);
+    const week = email.match(/([0-9]+)week(s{0,1})/);
+    const day = email.match(/([0-9]+)day(s{0,1})/);
     if (week != null && week.length > 1) return new Week(week[1]);
     if (day != null && day.length > 1) return new Day(day[1]);
     return null;
--- tests/test-classifier.js
+++ tests/test-classifier.js
@@ -3,7 +3,7 @@ var Classifier = require('../lib/classifier');
 var Week = require('../lib/week');
 var Day = require('../lib/day');
 
-test('test 1 week classifier', function (t){
+test('test 1 week classifier', function (t) {
   t.plan(2);
 
   var classifier = new Classifier();
@@ -12,6 +12,17 @@ test('test 1 week classifier', function (t){
 
   t.ok(type instanceof Week);
   t.equal(type.num, 1);
+});
+
+test('test 2 weeks classifier', function (t) {
+  t.plan(2);
+
+  var classifier = new Classifier();
+  var email = '2weeks@example.com';
+  var type = classifier.classify(email);
+
+  t.ok(type instanceof Week);
+  t.equal(type.num, 2);
 });
 
 test('test 1 day classifier', function (t) {
@@ -23,4 +34,15 @@ test('test 1 day classifier', function (t) {
 
   t.ok(type instanceof Day);
   t.equal(type.num, 1);
+});
+
+test('test 2 days classifier', function (t) {
+  t.plan(2);
+
+  var classifier = new Classifier();
+  var email = '2days@example.com';
+  var type = classifier.classify(email);
+
+  t.ok(type instanceof Day);
+  t.equal(type.num, 2);
 });
