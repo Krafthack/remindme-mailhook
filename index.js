166b0ddcf030fedde4cdb30dd9e9eb98a3d45fd9
Ken Grønnbeck <kengroenn@gmail.com>
2015-05-12 16:21:19 +0200

    Added webhook for mail

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 let express = require('express');
-var app = express();
+var bodyParser = require('body-parser')
+var multer = require('multer');
+var mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 8000;
+const MONGODB = process.env.MONGOLAB_URI || 'mongodb://localhost/remindme';
+
+mongoose.connect(MONGODB);
+
+var Event = mongoose.model('Event', {
+  type: String,
+  createdAt: Date,
+  payload: Object,
+  query: Object
+});
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(multer());
+
+app.post('/webhook/email', (req, res) => {
+  var event = new Event({type: 'Mail', createdAt: Date.now()});
+  event.payload = req.body;
+  event.save((err) => {
+    if (err) return res.status(500).send('ERROR');
+    else return res.send('OK');
+  });
+});
 
 app.get('/', (req, res) => res.send('Hello world'));
 
-app.listen(PORT, () => console.log('Server is running'));
+app.listen(PORT, () => console.log('Server is running at ' + PORT));
